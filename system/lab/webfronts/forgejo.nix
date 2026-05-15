88031cefcd075253fdb76284b74c6c2a6fe50de6
Trayambak Rai <xtrayambak@gmail.com>
2026-05-15 06:13:41 +0530

    forgejo: disable signups

--- system/lab/webfronts/forgejo.nix
+++ system/lab/webfronts/forgejo.nix
@@ -9,6 +9,11 @@
         DOMAIN = "lab.local";
         HTTP_PORT = 3000;
       };
+      service = {
+        DISABLE_REGISTRATION = true;
+        REQUIRE_SIGNIN_VIEW = true;
+        ENABLE_ROBOTS_TXT = true;
+      };
     };
 
     # Keep backing up stuff to the backup drive because I'm not very fond of losing my stuff
