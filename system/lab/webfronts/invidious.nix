9b81a78dc05f28584810a43363c86218a1a8d666
Trayambak Rai <xtrayambak@gmail.com>
2026-05-18 11:20:54 +0530

    invidious 1

--- system/lab/webfronts/invidious.nix
+++ system/lab/webfronts/invidious.nix
@@ -1,7 +1,40 @@
 { ... }:
+let
+  invidiousCompanionSecret = "iHa7riebeigeex6X";
+in
 {
   services.invidious = {
     enable = true;
     port = 3004;
+
+    http3-ytproxy.enable = true;
+    nginx.enable = true;
+
+    database = {
+      createLocally = true;
+    };
+
+    settings = {
+      invidious_companion.private_url = "http://127.0.0.1:8282";
+      invidious_companion_key = invidiousCompanionSecret;
+    };
+    sig-helper.enable = false; # Deprecated
+  };
+
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers = {
+      invidious-companion = {
+        image = "quay.io/invidious/invidious-companion:latest";
+        ports = [ "127.0.0.1:8282:8282" ];
+        volumes = [
+          "companioncache:/var/tmp/youtubei.js:rw"
+        ];
+        environment = {
+          # Same as configured on invidious above.
+          SERVER_SECRET_KEY = invidiousCompanionSecret;
+        };
+      };
+    };
   };
 }
