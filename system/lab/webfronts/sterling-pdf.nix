731f71cbb8a79c6fed81a3a32979a8bed78ee73e
Trayambak Rai <xtrayambak@gmail.com>
2026-05-20 22:18:50 +0530

    skibidi

--- system/lab/webfronts/sterling-pdf.nix
+++ system/lab/webfronts/sterling-pdf.nix
@@ -5,8 +5,6 @@
     environment = {
       LANGS = "en_GB";
       SECURITY_ENABLELOGIN = "true";
-      SECURITY_INITIALLOGIN_USERNAME = "admin";
-      SECURITY_INITIALLOGIN_PASSWORD = "flyingfishes1337";
       SYSTEM_MAXFILESIZE = 2000;
       SERVER_PORT = 3007;
     };
