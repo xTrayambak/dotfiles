398ea0f76deb3026312254c9043ccaf18c5347f4
Trayambak Rai <xtrayambak@gmail.com>
2026-05-21 15:37:02 +0530

    cockpit2

--- system/lab/services/cockpit.nix
+++ system/lab/services/cockpit.nix
@@ -1,12 +1,12 @@
+{ lib }:
 {
   services.cockpit = {
     enable = true;
     openFirewall = true;
-    port = 9090;
     settings = {
       WebService = {
         AllowUnencrypted = true;
-        Origins = "https://dash.xtrayambak.xyz";
+        Origins = lib.mkForce "https://dash.xtrayambak.xyz";
         ProtocolHeader = "X-Forwarded-Proto";
       };
       Session = {
