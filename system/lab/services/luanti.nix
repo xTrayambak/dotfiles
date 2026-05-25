71092b5c80a0b051bb79b56dcc9f08b887e5ccf0
Trayambak Rai <xtrayambak@gmail.com>
2026-05-25 14:59:19 +0530

    add: luanti stuff

--- system/lab/services/luanti.nix
+++ system/lab/services/luanti.nix
@@ -1,6 +1,53 @@
 { pkgs, ... }:
+let
+  gameId = "voxelibre";
+  worldName = "goodnightcraft";
+
+  voxelibreSrc = pkgs.fetchFromGithub {
+    owner = "VoxeLibre";
+    repo = "VoxeLibre";
+    rev = "0.91.2";
+    hash = "";
+  };
+in
 {
   environment.systemPackages = with pkgs; [
     luanti-server
   ];
+
+  users.users.gncraft = {
+    isNormalUser = true;
+    extraGroups = [ ];
+    initialPassword = "minetest";
+    shell = pkgs.bash;
+    openssh.authorizedKeys.keys = [ ];
+  };
+
+  systemd.services = {
+    gncraft = {
+      description = "GoodnightCraft Season 2";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+
+      preStart = ''
+        mkdir -p ~/.minetest/worlds/
+        mkdir -p ~/.minetest/games/
+
+        if [ ! -d ".minetest/games/${gameId}" ]; then
+          ln -sfn ${voxelibreSrc} ".minetest/games/${gameId}"
+        fi
+      '';
+
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.luanti-server}/bin/luantiserver --gameid ${gameId} --worldname ${worldName} --terminal";
+        User = "gncraft";
+
+        /*
+          Restart = "on-failure";
+          	RestartSec = "2s";
+        */
+      };
+    };
+  };
 }
