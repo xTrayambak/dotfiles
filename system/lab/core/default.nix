fc3705e852fde610584d4d10a93c7b1457c5dc13
Trayambak Rai <xtrayambak@gmail.com>
2026-05-25 13:00:32 +0530

    add: lab/core: security+hardening stuff

--- system/lab/core/default.nix
+++ system/lab/core/default.nix
@@ -2,8 +2,9 @@
 {
   imports = [
     ./boot.nix
-    ./users.nix
     ./chores.nix
+    ./security.nix
+    ./users.nix
   ];
 
   documentation = {
@@ -35,22 +36,6 @@
       "nix-switch" = "sudo nixos-rebuild switch --flake /etc/nixos#lab";
       "pull-conf" = "sudo git -C /etc/nixos pull origin master";
     };
-  };
-
-  boot.kernel.sysctl = {
-    # The Magic SysRq key is a key combo that allows users connected to the system console
-    # to perform some lowlevel commands. It's a security concern and we do not require it.
-    "kernel.sysrq" = 0;
-
-    # TCP Fast Open is a TCP extension that reduces network latency by packing
-    # data in the sender’s initial TCP SYN. Setting 3 = enable TCP Fast Open for
-    # both incoming and outgoing connections:
-    "net.ipv4.tcp_fastopen" = 3;
-
-    # cloudflared's quic-go dependency recommends setting the buffer
-    # sizes to ~7.5 MB to prevent warnings and other errors.
-    "net.core.rmem_max" = 7500000;
-    "net.core.wmem_max" = 7500000;
   };
 
   system.stateVersion = lib.mkForce "25.11";
