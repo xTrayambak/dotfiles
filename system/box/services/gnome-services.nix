af50ec0ea0b2df9cb729ebe706553fcc67c5fe47
Trayambak Rai <xtrayambak@gmail.com>
2026-05-31 10:23:00 +0530

    add: hello nixos 26.11

--- system/box/services/gnome-services.nix
+++ system/box/services/gnome-services.nix
@@ -1,17 +1,19 @@
 { pkgs, ... }:
 {
-  nixpkgs.overlays = [
-    (final: prev: {
-      gnome = prev.gnome.overrideScope (
-        gfinal: gprev: {
-          gvfs = gprev.gvfs.override {
-            googleSupport = true;
-            gnomeSupport = true;
-          };
-        }
-      );
-    })
-  ];
+  /*
+    nixpkgs.overlays = [
+      (final: prev: {
+        gnome = prev.gnome.overrideScope (
+          gfinal: gprev: {
+            gvfs = gprev.gvfs.override {
+              googleSupport = true;
+              gnomeSupport = true;
+            };
+          }
+        );
+      })
+    ];
+  */
   nixpkgs.config.permittedInsecurePackages = [ "libsoup-2.74.3" ];
   services = {
     dbus.packages = with pkgs; [
