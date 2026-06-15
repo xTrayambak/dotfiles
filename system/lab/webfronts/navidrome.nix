675bd6806c75a2c758db2dcfb4d5206bea9289e0
Trayambak Rai <xtrayambak@gmail.com>
2026-06-15 18:58:12 +0530

    navi2

--- system/lab/webfronts/navidrome.nix
+++ system/lab/webfronts/navidrome.nix
@@ -5,7 +5,12 @@
     settings = {
       Port = 3007;
       Address = "localhost";
+
       MusicFolder = "/pool/navidrome";
+      CacheFolder = "/pool/navidrome/cache";
+
+      Agents = "apple-music,deezer,lastfm,listenbrainz";
+
       EnableInsightsCollector = true; # help your local OSS project! ;)
     };
     plugins = with pkgs.navidromePlugins; [
