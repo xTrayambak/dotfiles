b98ad0ecda2e2dcadac4d1eee3c07732073e0224
Trayambak Rai <xtrayambak@gmail.com>
2026-06-21 09:19:01 +0530

    lab bootspec

--- system/lab/core/boot.nix
+++ system/lab/core/boot.nix
@@ -1,41 +1,40 @@
 { lib, pkgs, ... }:
 
 {
-	boot = {
-		bootspec.enable = true;
+  boot = {
 
-		initrd = {
-			systemd.enable = true;
-			supportedFilesystems = [ "ext4" ];
-			verbose = false;
-		};
+    initrd = {
+      systemd.enable = true;
+      supportedFilesystems = [ "ext4" ];
+      verbose = false;
+    };
 
-		kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
-		consoleLogLevel = 0;
+    kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
+    consoleLogLevel = 0;
 
-		kernelParams = [
-			"amd_iommu=on"
-			"amd-pstate=active"
-			"quiet"
-			"splash"
-			"boot.shell_on_fail"
-			"loglevel=3"
-			"rd.systemd.show_status=false"
-			"rd.udev.log_level=3"
-			"udev.log_priority=3"
-		];
+    kernelParams = [
+      "amd_iommu=on"
+      "amd-pstate=active"
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
 
-		loader = {
-			efi.canTouchEfiVariables = true;
-			systemd-boot.enable = true;
-		};
+    loader = {
+      efi.canTouchEfiVariables = true;
+      systemd-boot.enable = true;
+    };
 
-		plymouth = {
-			enable = true;
-			theme = "bgrt";
-			themePackages = with pkgs; [
-				nixos-bgrt-plymouth
-			];
-		};
-	};
+    plymouth = {
+      enable = true;
+      theme = "bgrt";
+      themePackages = with pkgs; [
+        nixos-bgrt-plymouth
+      ];
+    };
+  };
 }
