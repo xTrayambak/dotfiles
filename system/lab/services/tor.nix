c695ae4d94999a7e6c7086cab01b7ad3745e3d22
Trayambak Rai <xtrayambak@gmail.com>
2026-06-22 21:05:02 +0530

    tor2

--- system/lab/services/tor.nix
+++ system/lab/services/tor.nix
@@ -13,7 +13,7 @@
       ServerTransportPlugin = "obfs4 exec ${pkgs.lyrebird}/bin/lyrebird";
       ServerTransportListenAddr = "obfs4 0.0.0.0:443";
 
-      ExitRelay = lib.mkForce 0;
+      ExitRelay = lib.mkForce false;
 
       RelayBandwidthRate = "20 MBytes";
       RelayBandwidthBurst = "24 MBytes";
