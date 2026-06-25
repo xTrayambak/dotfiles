5c9637a0275181d0924f29b0c14cf19745d1777f
Trayambak Rai <xtrayambak@gmail.com>
2026-06-25 14:14:47 +0530

    tor

--- system/lab/services/tor.nix
+++ system/lab/services/tor.nix
@@ -1,25 +1,27 @@
 { lib, pkgs, ... }:
 {
-  services.tor = {
-    enable = true;
-    relay = {
+  /*
+    services.tor = {
       enable = true;
-      role = "relay";
+      relay = {
+        enable = true;
+        role = "relay";
+      };
+      settings = {
+        Nickname = "FriendsOfDorothy";
+        ORPort = 9001;
+
+        ControlPort = 9051;
+        CookieAuthentication = true;
+
+        # AssumeReachable = true;
+        ExitRelay = lib.mkForce false;
+
+        RelayBandwidthRate = "20 MBytes";
+        RelayBandwidthBurst = "24 MBytes";
+      };
     };
-    settings = {
-      Nickname = "FriendsOfDorothy";
-      ORPort = 9001;
-
-      ControlPort = 9051;
-      CookieAuthentication = true;
-
-      # AssumeReachable = true;
-      ExitRelay = lib.mkForce false;
-
-      RelayBandwidthRate = "20 MBytes";
-      RelayBandwidthBurst = "24 MBytes";
-    };
-  };
+  */
 
   services.snowflake-proxy = {
     enable = true;
