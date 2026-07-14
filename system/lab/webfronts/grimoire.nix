46b99b671f860afb957bb22a57e49fb3b4e9cfe1
Trayambak Rai <xtrayambak@gmail.com>
2026-07-14 17:11:06 +0530

    a

--- system/lab/webfronts/grimoire.nix
+++ system/lab/webfronts/grimoire.nix
@@ -2,7 +2,9 @@
 let
   port = "6767";
   rebuildScript = pkgs.writeShellScriptBin "grimoirevoncrimson" ''
+    export HOME=/home/nginx
     cd /home/nginx/grimoire
+
     # ${pkgs.git}/bin/git pull origin master
     ${pkgs.nimble}/bin/nimble build -d:release --opt:speed
     PORT=${port} ./grimoire
@@ -13,7 +15,10 @@ in
     enable = true;
     description = "Sherlocked thingmajig";
     serviceConfig = {
+      Type = "simple";
       User = "nginx";
+      Restart = "on-failure";
+      WorkingDirectory = "/home/nginx/grimoire";
       ExecStart = "${rebuildScript}/bin/grimoirevoncrimson";
     };
   };
