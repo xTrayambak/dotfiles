4aedd3cf02c656078b98b8fc607e6e2a5e5e23a6
Trayambak Rai <xtrayambak@gmail.com>
2026-07-17 23:08:21 +0530

    a

--- system/lab/webfronts/searxng.nix
+++ system/lab/webfronts/searxng.nix
@@ -1,4 +1,14 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }:
+let
+  environmentFile = pkgs.writeText "env" ''
+    SEARXNG_SECRET_KEY_FILE=${config.age.secrets.searxng.path}
+  '';
+in
+{
+  age.secrets.playit-secret = {
+    file = ../../../secrets/searxng.age;
+  };
+
   services.searx = {
     enable = true;
     package = pkgs.searxng;
@@ -9,5 +19,6 @@
         bind_address = "localhost";
       };
     };
+    environmentFile = environmentFile;
   };
 }
