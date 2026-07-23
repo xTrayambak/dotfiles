9b3abe1a64a2ad9a1df4d1654f2c43d022099fe9
Trayambak Rai <xtrayambak@gmail.com>
2026-07-23 22:48:27 +0530

    its time i kill myself

--- system/lab/services/virtualization.nix
+++ system/lab/services/virtualization.nix
@@ -13,30 +13,5 @@
         runroot = "/run/containers/storage";
       };
     };
-
-    oci-containers.containers = {
-      debian13 = {
-        image = "debian:13";
-        autoStart = true;
-
-        cmd = [
-          "bash"
-          "-c"
-          "while true; do if [ -f /grimoire/grimoire ]; then /grimoire/grimoire; else echo 'Waiting for binary...'; fi; sleep 5; done"
-        ];
-
-        ports = [ "3010:80" ];
-      };
-      sherlocked-bot = {
-        image = "debian:13";
-        autoStart = true;
-
-        cmd = [
-          "bash"
-          "-c"
-          "while true; do if [ -f /sherlocked-bot/src/bot.py ]; then python3 /sherlocked-bot/src/bot.py; else echo 'Waiting for bot'; fi; sleep 5; done"
-        ];
-      };
-    };
   };
 }
