78e67e9601f27162ecd00d4a4393c54b5a439040
Trayambak Rai <xtrayambak@gmail.com>
2026-08-08 01:27:55 +0530

    w security

--- system/lab/webfronts/kavita.nix
+++ system/lab/webfronts/kavita.nix
@@ -3,11 +3,11 @@
     enable = true;
     dataDir = "/pool/kavita";
     settings = {
-      TokenKey = "SdJ6avwSovPnI3pjl636uB7sx1Odwe0W1dMhP29vnighK1R2CcQFxNgjnI8jQxpqdrx72PiOkjO/OyDruAtNKw==";
       Port = 3012;
       IpAddresses = "0.0.0.0";
       BaseUrl = "/";
     };
+    tokenKeyFile = "/tmp/kavita.txt";
   };
 
   systemd.tmpfiles.rules = [
